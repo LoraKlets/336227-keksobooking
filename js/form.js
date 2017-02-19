893b257540382f9388f4bc12d441aa0d9a055fac
LoraKlets <lloraklets@gmail.com>
2017-02-19 13:33:26 +0900

    создание модулей showCard и synchronizeFields и возвращение фокуса на метку при закрытии карточки отеля

--- js/form.js
+++ js/form.js
@@ -2,8 +2,39 @@
 // При нажатии на элемент .pin ему будет добавляться класс .pin--active
 var tokyoPinMap = document.querySelector('.tokyo__pin-map');
 var dialog = document.querySelector('.dialog');
+var panelClose = dialog.querySelector('.dialog__close')
 
-window.initializePins(tokyoPinMap, dialog);
+window.dialogHandle(null);
+
+tokyoPinMap.addEventListener('click', function (evt) {
+  window.initializePins.elementHandler(evt);
+  window.dialogHandle(null);
+  });
+var onDialogCb = function () {
+  var activePin = tokyoPinMap.querySelector('.pin--active');
+  var imgActivePin = activePin.querySelector('img');
+  imgActivePin.focus();
+}
+tokyoPinMap.addEventListener('keydown', function (evt) {
+  if (window.initializePins.isActivateEvent(evt)) {
+      window.initializePins.elementHandler(evt);
+      window.dialogHandle(onDialogCb);
+  }
+});
+/*panelClose.addEventListener('click', function () {
+  window.initializePins.pinActiveRemove();
+  dialog.classList.add('invisible');
+});
+var onPanelClose = function () {
+
+};
+panelClose.addEventListener('keydown', function (evt) {
+  if (window.initializePins.isActivateEvent(evt)) {
+    window.initializePins.pinActiveRemove();
+    dialog.classList.add('invisible');
+    panelClose.setAttribute('aria-pressed', 'true');
+  }
+});*/
 
 var rentTitle = document.querySelector('#title');
 rentTitle.required = true;
@@ -17,8 +48,17 @@ rentPrice.required = true;
 rentPrice.max = 1000000;
 rentPrice.min = 1000;
 
-window.synchronizeFields(document.querySelector('#time'), document.querySelector('#timeout'), ['12', '13', '14'], ['12', '13', '14'], 'value');
 
-window.synchronizeFields(document.querySelector('#type'), document.querySelector('#price'), ['квартира', 'лачуга', 'дворец'], ['1000', '0', '10000'], 'min');
+var syncValues = function (element, value) {
+  element.value = value;
+};
+// синхронизация двусторонняя - вызываем дважды
+window.synchronizeFields(document.querySelector('#time'), document.querySelector('#timeout'), ['12', '13', '14'], ['12', '13', '14'], syncValues);
+window.synchronizeFields(document.querySelector('#timeout'), document.querySelector('#time'), ['12', '13', '14'], ['12', '13', '14'], syncValues);
+// синхронизируем количество комнат и число гостей
+window.synchronizeFields(document.querySelector('#room_number'), document.querySelector('#capacity'), ['1', '2', '3'], ['не для гостей', 'для 3 гостей', 'для 3 гостей'], syncValues);
 
-window.synchronizeFields(document.querySelector('#room_number'), document.querySelector('#capacity'), ['1 комната', '2 комнаты', '100 комнат'], ['не для гостей', 'для 3 гостей', 'для 3 гостей'], 'value');
+var syncValueWithMin = function(element, value) {
+  element.min = value;
+};
+window.synchronizeFields(document.querySelector('#type'), document.querySelector('#price'), ['квартира', 'лачуга', 'дворец'], ['1000', '0', '10000'], syncValueWithMin);
---
+++ js/show-card.js
@@ -0,0 +1,8 @@
+'use strict';
+window.showCard = (function () {
+  var dialog = document.querySelector('.dialog');
+  var dialogTitle = dialog.querySelector('img');
+  var activePin = tokyoPinMap.querySelector('.pin--active');
+  var imgActivePin = activePin.querySelector('img');
+  dialogTitle.src = imgActivePin.src; 
+});
